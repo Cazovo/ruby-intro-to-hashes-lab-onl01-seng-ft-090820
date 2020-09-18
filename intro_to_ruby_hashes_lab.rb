8e5f2a1e35b09f03d671b545cfdcfbeedfe7c75f
conner fortner <fortner.conner@yahoo.com>
2020-09-18 20:08:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,8 +33,12 @@ def monopoly_with_third_tier
 	             :pennsylvania_railroad => {},
 	             :b_and_o_railroad => {},
 	             :shortline => {}
+	  },
+	  :rent_in_dollars=>{:one_piece_owned => 25,
+	                     :two_piece_owned => 50,
+	                     :third_piece_owned => 100,
+	                     :four_piece_owned => 200
 	  }
-	  :rent_in_dollars=>{}
 	   
 	 }
 	 }
