5cbb5e8d8bf80b055fc8753b321d4e79a6652136
conner fortner <fortner.conner@yahoo.com>
2020-09-18 20:03:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,10 @@ def monopoly_with_second_tier
 	 monopoly = {:railroads =>
 	 {:pieces => 4, 
 	  :names => {}
-	  :rent_in_dollars=>{}}
+	  :rent_in_dollars=>{}
+	   
+	 }
+	 }
 	 
 	  
 
@@ -25,8 +28,6 @@ end
 
 def monopoly_with_third_tier
     
-     
-   }
 
 end
 
